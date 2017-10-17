eb99e111a85658d2108f579cb51b642f1cdf31c6
sukeshsangam <ssang001@odu.edu>
2017-10-17 12:29:15 -0400

    Database updated

--- milestone1dump.sql
+++ milestone1dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 17, 2017 at 02:11 AM
+-- Generation Time: Oct 17, 2017 at 04:25 PM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -27,8 +27,11 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `channels`
 --
+
 CREATE DATABASE `slack_lamp_stack_518`;
 USE slack_lamp_stack_518;
+
+
 
 DROP TABLE IF EXISTS `channels`;
 CREATE TABLE IF NOT EXISTS `channels` (
@@ -72,79 +75,27 @@ CREATE TABLE IF NOT EXISTS `channel_messages` (
   PRIMARY KEY (`message_id`),
   KEY `channel_id_messaged` (`channel_id`),
   KEY `user_id_messages` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `channel_messages`
 --
 
 INSERT INTO `channel_messages` (`message_id`, `channel_id`, `user_id`, `message`, `timestamp`, `smileys`) VALUES
-(1, 1, 1, 'hello this is my first message', '2017-10-10 09:47:45', 'True'),
-(2, 1, 1, 'hi dude', '2017-10-10 09:49:32', 'True'),
-(3, 1, 1, 'hello', '2017-10-10 09:51:05', 'True'),
-(4, 1, 1, 'hello hi', '2017-10-10 09:52:28', 'True'),
-(5, 1, 1, 'hey', '2017-10-10 09:53:00', 'True'),
-(6, 1, 1, 'hey\'\'\'', '2017-10-10 09:53:15', 'True'),
-(7, 1, 1, 'hey\'', '2017-10-10 09:53:24', 'True'),
-(8, 1, 1, 'hey this is sukesh', '2017-10-10 09:53:58', 'False'),
-(9, 1, 1, 'hello this is general', '2017-10-10 10:03:08', 'False'),
-(10, 1, 1, 'testing', '2017-10-10 10:04:44', 'False'),
-(11, 1, 1, 'hello', '2017-10-10 11:13:20', 'False'),
-(12, 1, 1, 'hello', '2017-10-10 11:13:29', 'False'),
-(13, 1, 1, 'hello', '2017-10-10 11:13:38', 'False'),
-(14, 1, 1, 'hello', '2017-10-10 11:13:52', 'False'),
-(15, 1, 1, 'namaste', '2017-10-10 11:13:59', 'False'),
-(16, 2, 1, 'helloe', '2017-10-10 11:19:43', 'False'),
-(17, 2, 1, 'hello', '2017-10-10 11:20:07', 'False'),
-(18, 2, 1, 'hi', '2017-10-10 11:25:44', 'False'),
-(19, 2, 1, 'hi', '2017-10-10 11:25:45', 'False'),
-(20, 2, 1, 'hi', '2017-10-10 11:25:59', 'False'),
-(21, 1, 1, 'testing', '2017-10-11 00:03:59', 'False'),
-(22, 1, 1, 'rdfgh', '2017-10-11 02:11:07', 'False'),
-(23, 1, 1, 'fgthfdgyj y', '2017-10-11 02:11:17', 'False'),
-(24, 1, 1, 'kjijljkl', '2017-10-11 02:11:27', 'False'),
-(25, 1, 1, ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,jklkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkkkkkkk', '2017-10-11 02:11:58', 'False'),
-(26, 1, 1, 'kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk', '2017-10-11 02:16:57', 'False'),
-(27, 1, 1, '<!--hjgjyh-->', '2017-10-11 02:27:08', 'False'),
-(28, 1, 1, '<!--hjgjyh-->', '2017-10-11 02:27:09', 'False'),
-(29, 1, 1, 'I\'m', '2017-10-11 02:40:50', 'False'),
-(30, 1, 1, '<!--hjgjyh-->', '2017-10-11 02:41:04', 'False'),
-(31, 1, 4, 'hello', '2017-10-17 00:24:46', 'False'),
-(32, 4, 4, 'this is milestone1\r\n', '2017-10-17 00:42:24', 'False'),
-(33, 4, 4, 'this is milestone1\r\n', '2017-10-17 00:42:25', 'False'),
-(34, 5, 4, 'hhh', '2017-10-17 00:42:48', 'False'),
-(35, 5, 4, 'hello', '2017-10-17 00:49:28', 'False'),
-(36, 4, 4, 'dude', '2017-10-17 00:50:23', 'False'),
-(37, 4, 4, 'dude', '2017-10-17 00:51:24', 'False'),
-(38, 4, 4, 'hello', '2017-10-17 00:52:44', 'False'),
-(39, 4, 4, 'hello', '2017-10-17 00:53:41', 'False'),
-(40, 4, 4, 'hello', '2017-10-17 00:55:17', 'False'),
-(41, 4, 4, 'hello', '2017-10-17 00:55:41', 'False'),
-(42, 3, 4, 'hello', '2017-10-17 00:55:55', 'False'),
-(43, 3, 4, 'nice its working', '2017-10-17 00:56:11', 'False'),
-(44, 5, 4, 'nice work', '2017-10-17 00:56:24', 'False'),
-(45, 2, 4, 'how are you?\r\n', '2017-10-17 00:57:30', 'False'),
-(46, 1, 4, 'I am sukku. Nice to meet u', '2017-10-17 00:57:51', 'False'),
-(47, 1, 4, 'good', '2017-10-17 01:01:08', 'False'),
-(48, 2, 1, 'fine', '2017-10-17 01:02:24', 'False'),
-(49, 2, 1, 'hau \r\ni \r\nam \r\ntoo\r\ngood\r\n', '2017-10-17 01:02:41', 'False'),
-(50, 2, 1, 'h\r\nh\r\nh\r\nh', '2017-10-17 01:02:57', 'False'),
-(51, 4, 7, 'namste guru\r\n', '2017-10-17 01:06:30', 'False'),
-(52, 2, 7, 'random it is', '2017-10-17 01:06:43', 'False'),
-(53, 1, 8, 'done my work', '2017-10-17 01:35:32', 'False'),
-(54, 1, 9, 'okay nice job\r\n', '2017-10-17 01:40:00', 'False'),
-(55, 5, 10, 'what r u talking', '2017-10-17 01:40:35', 'False'),
-(56, 2, 10, 'how is norfolk', '2017-10-17 01:40:55', 'False'),
-(57, 2, 10, 'how is norfolk', '2017-10-17 01:40:57', 'False'),
-(58, 1, 1, '', '2017-10-17 01:41:39', 'False'),
-(59, 1, 1, '', '2017-10-17 01:45:24', 'False'),
-(60, 1, 1, '', '2017-10-17 01:45:45', 'False'),
-(61, 1, 1, '', '2017-10-17 01:45:49', 'False'),
-(62, 1, 1, '', '2017-10-17 01:45:57', 'False'),
-(63, 3, 1, '', '2017-10-17 01:46:07', 'False'),
-(64, 1, 1, '', '2017-10-17 01:49:53', 'False'),
-(65, 1, 1, '', '2017-10-17 01:49:57', 'False'),
-(66, 2, 1, 'ljkjkl', '2017-10-17 01:50:40', 'False');
+(1, 1, 1, 'Hello, hi all', '2017-10-14 04:00:00', 'false'),
+(2, 2, 1, 'Hello, hi all', '2017-10-14 04:00:00', 'false'),
+(3, 3, 1, 'Hello, hi all!', '2017-10-14 04:00:00', 'false'),
+(4, 4, 1, 'hello!', '2017-10-14 04:00:00', 'false'),
+(5, 1, 8, 'hello', '2017-10-14 04:00:00', 'false'),
+(6, 2, 8, 'hello', '2017-10-14 04:00:00', 'false'),
+(7, 4, 8, 'hello', '2017-10-14 04:00:00', 'false'),
+(8, 5, 8, 'hello', '2017-10-14 04:00:00', 'false'),
+(9, 4, 1, 'Can some provide me good tutorials for php', '2017-10-17 16:13:29', 'False'),
+(10, 1, 1, 'can some one provide me good tutorials for php?', '2017-10-17 16:14:42', 'False'),
+(11, 1, 4, 'Hello , this may help you https://www.tutorialspoint.com/php/', '2017-10-17 16:15:54', 'True'),
+(12, 2, 4, 'Hi ', '2017-10-17 16:17:05', 'False'),
+(13, 5, 9, 'Hi all, can some one provide me good tutorials on sql', '2017-10-16 05:01:00', 'false'),
+(14, 3, 9, 'hi , all this is testing message', '2017-10-16 05:00:00', 'false');
 
 -- --------------------------------------------------------
 
