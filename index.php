8e74644a8e8a9c6c0a347645c3d39e7f0111778d
cndhureddy <31669603+cndhureddy@users.noreply.github.com>
2017-11-21 14:56:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <div ><img id="logo"  src="images/logo.jpg"></div>
     <div class="logo-text">ODU CS Slack</div>
     <div class="login-button"><form><input id="signin-button" type="submit" value="Sign in"></form></div>
-    <div class="login-button"><form><input id="signin-button" type="submit" value="Help"></form></div>
+    <div class="login-button"><form action="help.php"><input id="signin-button" type="submit" value="Help"></form></div>
 </div>
 
 <div id="login-center">
