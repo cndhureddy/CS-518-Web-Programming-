c7b3ac84f8be4fcb4b987d29e2ad4bfd8efe1a67
sshya <31662008+sshya@users.noreply.github.com>
2017-11-21 14:59:11 -0500

    help page

---
+++ help.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ODU CS Slack</title>
+    <link rel="stylesheet" type="text/css" href="../css/registration.css">
+
+</head>
+<body>
+<div id="topbar" >
+    <div ><img id="logo"  src="../images/logo.jpg"></div>
+    <div class="logo-text">ODU CS Slack</div>
+ <!--   <div class="login-button" ><form action="login.php" ><input id="signin-button" type="submit" value="Sign in"></form></div> -->
+</div>
+
+
+<!--start: Container -->
+	<div class="container">
+			<!--start: Wrapper-->
+		<div id="wrapper">
+		<!-- start: Row -->
+		 	<div class="row-fluid">
+			<!-- start: 9 Grids -->
+			<div class="span9">
+			<center>
+			<h3>
+			Popular Topics
+			</h3>
+			</center>
+			<hr>
+                <center>
+			<table class="table">
+							<thead>
+        <tr>
+	       <td><a href="#Topic1"> Basic Usage </a></td>
+			<td></td>
+            <td></td>
+            <td></td>
+            <td><a href="#Topic2"> FAQs </a></td>	
+        </tr>
+		 </thead>
+			</table>
+                    </center>
+<HR width="100%">
+
+
+<A name=#Topic1></A><p><font face="arial, helvetica, sans-serif" color="#2161E1"><strong> Basic Usage </font></strong><p>
+<HR width="100%">
+
+   
+    
+<!--start: Container -->
+	<div class="container">
+			<!--start: Wrapper-->
+		<div id="wrapper">
+		<!-- start: Row -->
+		 	<div class="row-fluid">
+			<!-- start: 9 Grids -->
+			<div class="span9">
+			<center>
+			<h3>
+			Questions You May Have Regarding Basic Usage
+			</h3>
+            
+			</center>
+			<hr>
+			<table class="table">
+							<thead> </thead>
+            <tr> <td><a href="#Question11">How do I register? </a></td> </tr>
+			<tr> <td><a href="#Question12">How do I Login? </a></td> </tr>
+            <tr> <td><a href="#Question14"> Uploading profile Picture </a></td> </tr>
+            <tr> <td><a href="#Question15"> How to create Channels?</a></td> </tr>
+                <tr> <td><a href="#Question16">Invite members to channels</a></td> </tr>
+                <tr> <td><a href="#Question17">How do I Sign out?</a></td> </tr>
+                <tr> <td><a href="#Question18">How to react to posts?</a></td> </tr>
+                <tr> <td><a href="#Question19">How do I post messages in channels ?</a></td> </tr>
+				 <tr> <td><a href="#Question20">How to view my profile details ?</a></td> </tr>
+		
+							</table>
+<HR width="100%">
+
+
+<A name=Question11></A><p><font face="arial, helvetica, sans-serif" color="#990000"><strong>How do I register? </font></strong><p>
+<HR width="100%">
+
+<br>
+<ol>
+  <li>Go to <a href= "http://sukeshsangam.cs518.cs.odu.edu/index.php">Login page</a> and Click Register or</li>
+  <li>Go to <a href= "http://sukeshsangam.cs518.cs.odu.edu/php/registration.php">Registration page</a>">login page</a></li>
+  <li>Enter details. If username is not unique, it throws error. Try entering a new username. Also, password should be of 6-20 characters length</li>
+  <li>Click Register</li>
+</ol> 
+    <br>
+ <hr> 
+ 
+ 
+
+ <A name=Question12></A><br><p><font face="arial, helvetica, sans-serif" color="#990000"><strong>How do I Login? </font></strong><p>
+<HR width="100%">
+ 
+<br>
+<ol>
+  <li>Go to <a href= "http://sukeshsangam.cs518.cs.odu.edu/index.php">Login page</a></li>
+  <li>Enter your username and password</li>
+  <li>Click  Submit</li>
+</ol> 
+    <br> 
+	 <hr> 
+ 
+<A name=Question14></A><p><font face="arial, helvetica, sans-serif" color="#990000"><strong>Uploading profile Picture</font></strong><p>
+<HR width="100%">
+
+<ol>
+  <li>Login to your account</li>
+  <li>Click on the dropdown button on the top-left corner(where workspace name is displayed)of the home page</li>
+  <li>From the dropdown list, select "Profile & account" option and it directs to a page </li>
+  <li>Click on the "Choose File" option and browse the profile picture. The picture should have minimum height and width of 250 px and maximum height and width of 750 px</li>
+  <li>Click update picture option </li>
+</ol> 
+ <hr> 
+
+ <A name=Question15></A><br><p><font face="arial, helvetica, sans-serif" color="#990000"><strong> How to Create Channels</font></strong><p>
+<HR width="100%">
+ 
+  <ol>
+  <li>Login to your account</li>
+  <li>Click on the Channels + option on the left side of the page</li>
+  <li>Select a unique channel name with 5-100 characters</li>
+  <li>Choose public or private option from the dropdown menu of privacy setting accoring to your requirement. </li>
+  <li>You can describe purpose of the channel under purpose option. It is not mandatory </li>
+ <li>Click Submit</li>
+</ol> 
+ <hr> 
+
+
+
+ <A name=Question16></A><br><p><font face="arial, helvetica, sans-serif" color="#990000"><strong>How to Invite members to channels? </font></strong><p>
+<HR width="100%">
+ 
+<ol>
+  <li>Enter the channel by selecting the channel from left side bar of the home page</li>
+  <li>Click on the invite_members button displayed on the top of the channel page </li>
+  <li>You can see list of members that can be invited with "invite member" option beside their user names which should be clicked to invite a member</li>
+  
+</ol> 
+ <hr> 
+ 
+ <A name=Question17></A><br><p><font face="arial, helvetica, sans-serif" color="#990000"><strong>How do I Sign out?</font></strong><p>
+<HR width="100%">
+ 
+<ol>
+  <li>Click on the dropdown button on the top-left corner(where workspace name is displayed)of the home page</li>
+  <li>From the dropdown list, select "Sign Out of slack cs odu." option and it directs to a page </li>
+</ol> 
+ <hr>  
+ 
+ <A name=Question18></A><br><p><font face="arial, helvetica, sans-serif" color="#990000"><strong>How to react to posts in a channel?</font></strong><p>
+<HR width="100%">
+ 
+<ol>
+  <li>When you open a particular channel, you see posts from various users.</li>
+  <li> Beside the post you can find like, dislike and reply icons. Use these icons to react to the post </li>
+   <li>By selecting reply icon, a window pops up where you can enter message in the text box and then click on "send" to send it</li>
+</ol> 
+ <hr>  
+ <A name=Question19></A><br><p><font face="arial, helvetica, sans-serif" color="#990000"><strong>How do I post messages in channels ?</font></strong><p>
+<HR width="100%">
+<ol>
+  <li>When you open a particular channel, you see a text box in the bottom of the page.</li>
+  <li>Enter the message in the text box and click on the send icon beside the text box to post the message. </li>
+   <li>You can post code snippet, image or url to the channel by selecting the respective icon in the bottom right corner of the page and then by clicking on the send icon. </li>
+</ol> 
+ <hr>  
+ <br>
+
+<A name=Question20></A><p><font face="arial, helvetica, sans-serif" color="#990000"><strong>How to view my profile details ?</font></strong><p>
+<HR width="100%">
+
+<ol>
+  <li>Login to your account</li>
+  <li>Click on the dropdown button on the top-left corner(where workspace name is displayed)of the home page</li>
+  <li>From the dropdown list, select "Profile & account" option and it directs to a page </li>
+  <li>You can see the profile details</li>
+</ol> 
+ <hr> 
+
+
+			
+			</div>
+			<!-- end: 9 Grids -->
+			
+			<!-- end: Row -->
+			</div>
+			<!-- end: Row -->
+		</div>
+		<!-- end: Wrapper  -->
+			
+	</div>
+<!--end: Container-->
+    
+    
+    
+ <A name=Topic2></A><br><p><font face="arial, helvetica, sans-serif" color="#2161E1"><strong>FAQs - Frequently Asked Questions</font></strong><p>
+<HR width="100%">
+ 
+
+<!--start: Container -->
+	<div class="container">
+			<!--start: Wrapper-->
+		<div id="wrapper">
+		<!-- start: Row -->
+		 	<div class="row-fluid">
+			<!-- start: 9 Grids -->
+			<div class="span9">
+			<center>
+			<h3>
+			Questions you may have
+			</h3>
+			</center>
+			<hr>
+			<table class="table">
+							<thead> </thead>
+            <tr> <td><a href="#Question1">I can't log in </a></td> </tr>
+			<tr> <td><a href="#Question2">I can't upload profile picture </a></td> </tr>
+            <tr> <td><a href="#Question3">How to manage error while registering </a></td> </tr>
+        
+		
+							</table>
+<HR width="100%">
+
+
+<A name=Question1></A><p><font face="arial, helvetica, sans-serif" color="#990000"><strong> I can't log in</font></strong><p>
+<HR width="100%">
+
+<ol>
+  <li>Enter the same user name and password  used for registeration.</li>
+ 
+</ol>
+ <hr> 
+ 
+ 
+
+ <A name=Question2></A><br><p><font face="arial, helvetica, sans-serif" color="#990000"><strong>I can't upload profile picture  </font></strong><p>
+<HR width="100%">
+ 
+<ol>
+  <li>The picture should have minimum height and width of 250 px and maximum height and width of 750 px</li>
+ 
+</ol>
+ <hr> 
+ 
+<A name=Question3></A><p><font face="arial, helvetica, sans-serif" color="#990000"><strong>How to manage error while registering</font></strong><p>
+<HR width="100%">
+
+<ol>
+  <li>If username is not unique, it throws error. Try entering a new username. Also, password should be of 6-20 characters length</li>
+ 
+</ol>
+ <hr> 
+
+    
+
+
+			
+			</div>
+			<!-- end: 9 Grids -->
+			
+			<!-- end: Row -->
+			</div>
+			<!-- end: Row -->
+		</div>
+		<!-- end: Wrapper  -->
+			
+	</div>
+<!--end: Container-->
+
+	
+	
+
+    
+    
+    	</div>
+			<!-- end: 9 Grids -->
+			
+			<!-- end: Row -->
+			</div>
+			<!-- end: Row -->
+		</div>
+		<!-- end: Wrapper  -->
+			
+	</div>
+<!--end: Container-->
+
+	
