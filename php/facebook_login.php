cfdcc62e332ac7c6cd267efec5f3f95f2251ac36
sukeshsangam <ssang001@odu.edu>
2017-12-10 02:11:23 -0500

    Update facebook_login.php

--- php/facebook_login.php
+++ php/facebook_login.php
@@ -5,7 +5,10 @@
  * Date: 12/10/2017
  * Time: 1:45 AM
  */
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
 echo GET["user_id"];
 echo GET["user_email"];
 echo GET["image_src"];
