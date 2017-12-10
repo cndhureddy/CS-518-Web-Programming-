2954731708f8fe30970fb91170b31119c6564484
sukeshsangam <ssang001@odu.edu>
2017-12-10 02:02:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-session.start();
+session_start();
     ?>
     <div id="topbar" >
     <div ><img id="logo"  src="images/logo.jpg"></div>
