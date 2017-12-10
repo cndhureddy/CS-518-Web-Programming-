3bdc44165d0909d54b63c13a913c2f7b1d747afd
sukeshsangam <ssang001@odu.edu>
2017-12-09 23:20:27 -0500

    Update example_twitter.php

--- example_twitter.php
+++ example_twitter.php
@@ -16,5 +16,5 @@ if (!isset($_SESSION['access_token'])) {
 	$access_token = $_SESSION['access_token'];
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 	$user = $connection->get("account/verify_credentials");
-	echo $user->screen_name;
+	echo $user;
 }
