2b9f1733476c36f79b0189b22d530754caf5913b
sukeshsangam <ssang001@odu.edu>
2017-12-09 23:27:14 -0500

    Update example_twitter.php

--- example_twitter.php
+++ example_twitter.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
 require 'autoload.php';
 use Abraham\TwitterOAuth\TwitterOAuth;
@@ -11,7 +13,8 @@ if (!isset($_SESSION['access_token'])) {
 	$_SESSION['oauth_token'] = $request_token['oauth_token'];
 	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
 	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
-	echo $url;
+	//echo $url;
+header( 'Location: ' . $url );
 } else {
 	$access_token = $_SESSION['access_token'];
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
