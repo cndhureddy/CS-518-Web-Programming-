31fd5366308d9bd049895e485aaf2a398fd97cbb
sukeshsangam <ssang001@odu.edu>
2017-12-10 02:18:27 -0500

    Update facebook_login.php

--- php/facebook_login.php
+++ php/facebook_login.php
@@ -1,16 +1,3 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ssangam
- * Date: 12/10/2017
- * Time: 1:45 AM
- */
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-session_start();
-echo GET["user_id"];
-echo GET["user_email"];
-echo GET["image_src"];
-
+echo "hello";
 ?>
