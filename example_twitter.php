ccf17f3d425ec77cabab36021cf114b7ea3a31fa
sukeshsangam <ssang001@odu.edu>
2017-12-09 23:28:55 -0500

    Update example_twitter.php

--- example_twitter.php
+++ example_twitter.php
@@ -19,5 +19,5 @@ header( 'Location: ' . $url );
 	$access_token = $_SESSION['access_token'];
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 	$user = $connection->get("account/verify_credentials");
-	echo $user;
+	echo json_encode($user);
 }
