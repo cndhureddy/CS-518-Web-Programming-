562d927c64606b84b20ff3a8d3c8f5c42d3684a9
sukeshsangam <ssang001@odu.edu>
2017-12-09 23:20:08 -0500

    Update example2.php

--- example2.php
+++ example2.php
@@ -1,30 +1,14 @@
 <?php
-require_once('TwitterAPIExchange.php');
-/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
-$settings = array(
-'oauth_access_token' => "237713038-kxslmkUwtvQcAou8laa2Lq2ZFEO3sjCIhe93sZjd",
-'oauth_access_token_secret' => "	qP98R5LPl3YBioyiduo9FvEXmNv8qMyGRt0U2VEKdIC88",
-'consumer_key' => "	Uz1Zi6FvOYrUZV5e8CoSSTrEu",
-'consumer_secret' => "CI5xrBJaZI1imAWhSUA4QSSv8rXs9HyWz6JgKPMoNGl7hxlb2U"
-);
-$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
-$requestMethod = "GET";
-if (isset($_GET['user'])) {$user = $_GET['user'];} else {$user = "iagdotme";}
-if (isset($_GET['count'])) {$count = $_GET['count'];} else {$count = 20;}
-$getfield = "?screen_name=$user&count=$count";
-$twitter = new TwitterAPIExchange($settings);
-$string = json_decode($twitter->setGetfield($getfield)
-->buildOauth($url, $requestMethod)
-->performRequest(),$assoc = TRUE);
-if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
-foreach($string as $items)
-    {
-        echo "Time and Date of Tweet: ".$items['created_at']."<br />";
-        echo "Tweet: ". $items['text']."<br />";
-        echo "Tweeted by: ". $items['user']['name']."<br />";
-        echo "Screen name: ". $items['user']['screen_name']."<br />";
-        echo "Followers: ". $items['user']['followers_count']."<br />";
-        echo "Friends: ". $items['user']['friends_count']."<br />";
-        echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
-    }
-?>
+session_start();
+require 'autoload.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+if (isset($_REQUEST['oauth_verifier'], $_REQUEST['oauth_token']) && $_REQUEST['oauth_token'] == $_SESSION['oauth_token']) {
+	$request_token = [];
+	$request_token['oauth_token'] = $_SESSION['oauth_token'];
+	$request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
+	$access_token = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
+	$_SESSION['access_token'] = $access_token;
+	// redirect user back to index page
+	header('Location: ./example_twitter.php');
+}
