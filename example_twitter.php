febd64feb154ad46ca74928b4ddb7b82365d40a7
sukeshsangam <ssang001@odu.edu>
2017-12-09 23:18:52 -0500

    Create example_twitter.php

---
+++ example_twitter.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+require 'autoload.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+define('CONSUMER_KEY', 'Uz1Zi6FvOYrUZV5e8CoSSTrEu'); // add your app consumer key between single quotes
+define('CONSUMER_SECRET', 'CI5xrBJaZI1imAWhSUA4QSSv8rXs9HyWz6JgKPMoNGl7hxlb2U'); // add your app consumer secret key between single quotes
+define('OAUTH_CALLBACK', '	http://sukeshsangam.cs518.cs.odu.edu/example2.php'); // your app callback URL
+if (!isset($_SESSION['access_token'])) {
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+	$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
+	$_SESSION['oauth_token'] = $request_token['oauth_token'];
+	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
+	echo $url;
+} else {
+	$access_token = $_SESSION['access_token'];
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+	$user = $connection->get("account/verify_credentials");
+	echo $user->screen_name;
+}
