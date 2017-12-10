dafd16b14b92af7f207da9d4abbfa764af8cbcc2
sukeshsangam <sukesh.sangam@gmail.com>
2017-12-10 00:41:36 -0500

    css update & js

--- javascript/onchange_jquery.js
+++ javascript/onchange_jquery.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-
+    //$(".Suggestions").empty();
     $("body").on('click',function () {
         $(".Suggestions").empty();
     });
@@ -8,31 +8,36 @@ $( document ).ready(function() {
 
 dat=$(".enter_username").val();
 //console.log(dat);
+if(dat!="") {
+    $.ajax({
 
-        $.ajax({
+        type: "POST",
+        url: "../php/user_query_onselect.php",
+        data: {"key": dat},
+        dataType: 'json',
+        async: false,
+        success: function (data) {
+            // $(".chat_area").html("");
+            //$(".older_messages").remove();
+            //$(".chat_area").prepend(data);
+            //console.log(data);
+            $(".Suggestions").empty();
+            str = "<ul class=\"Suggestions_ul\">"
 
-            type: "POST",
-            url: "../php/user_query_onselect.php",
-            data: {"key": dat},
-            dataType: 'json',
-            async: false,
-            success: function (data) {
-                // $(".chat_area").html("");
-                //$(".older_messages").remove();
-                //$(".chat_area").prepend(data);
-                //console.log(data);
-                $(".Suggestions").empty();
-                str="<ul class=\"Suggestions_ul\">"
-
-                for (i in data) {
-                    //$(".Suggestions").append("<li id="+data[i]["user_id"]+">"+ data[i]["display_name"] +"</li>");
-                str=str+"<a id="+data[i]["user_id"]+" href=\" ..\\php\\get_profiles.php?user_id="+data[i]["user_id"]+"\">"+ data[i]["display_name"] +"</a><br>";
-                }
-                str=str+"</ul>";
-                $(".Suggestions").append(str);
+            for (i in data) {
+                //$(".Suggestions").append("<li id="+data[i]["user_id"]+">"+ data[i]["display_name"] +"</li>");
+                str = str + "<a id=" + data[i]["user_id"] + " href=\" ..\\php\\get_profiles.php?user_id=" + data[i]["user_id"] + "\">" + data[i]["display_name"] + "</a><br>";
             }
-        });
+            str = str + "</ul>";
+            $(".Suggestions").append(str);
+        }
+    });
+}
+else{
 
+    $(".Suggestions").empty();
+
+}
     });
 
 
