aa796dbf0ae22a791c074288d447c8147efcb247
sukeshsangam <ssang001@odu.edu>
2017-12-10 19:34:08 -0500

    Update example_twitter.php

--- example_twitter.php
+++ example_twitter.php
@@ -12,13 +12,13 @@ if (!isset($_SESSION['access_token'])) {
 	$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
 	$_SESSION['oauth_token'] = $request_token['oauth_token'];
 	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
-	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
+	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token'],'force_login=>true'),);
 	//echo $url;
 header( 'Location: ' . $url );
 } else {
 	$access_token = $_SESSION['access_token'];
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
-	$params = array('include_email' => 'true', 'include_entities' => 'false', 'skip_status' => 'true','force_login'=>true);
+	$params = array('include_email' => 'true', 'include_entities' => 'false', 'skip_status' => 'true','force_login'=>'true');
 	$user = $connection->get("account/verify_credentials",$params);
 	//echo "<pre>";
 	$user_name=$user->name;
