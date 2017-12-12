633ca494ca7c18eee646f1d015f547435abbcc47
cndhureddy <31669603+cndhureddy@users.noreply.github.com>
2017-12-11 23:41:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -143,6 +143,9 @@ session_start();
 
             <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
             </fb:login-button>
+            <form action="example_twitter.php">
+                <input type="submit" value="twitter login"/>
+            </form>
           <!--  <img id="profile_pic" src="//graph.facebook.com/{+final_user_id+}/picture?type=large">-->
             <div id="status">
 
