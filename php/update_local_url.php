118e42098fa69d8a71f4473fffbd45f6241a1224
sshya <31662008+sshya@users.noreply.github.com>
2017-12-12 06:10:46 -0500

    Pushing files

---
+++ php/update_local_url.php
@@ -0,0 +1,13 @@
+<?php
+
+include("connect.php");
+$email=$_SESSION["email"];
+$query_1="select * from dp_urls where email_id='".$email."'";
+$result_1=$conn->$query_1;
+$row_1=$result_1->fetch_assoc();
+$to_update=$row_1["local_url"];
+
+$query="update users set picture='".$to_update."'";
+mysqli_query($conn,$query);
+header("location:userprofile.php");
+mysqli_close($conn);
