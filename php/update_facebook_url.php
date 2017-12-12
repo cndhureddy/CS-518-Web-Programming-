e0e55d7e7141c21e3a85575be792a5fedc4c867f
sshya <31662008+sshya@users.noreply.github.com>
2017-12-12 06:29:39 -0500

    update

--- php/update_facebook_url.php
+++ php/update_facebook_url.php
@@ -1,14 +1,19 @@
 <?php
-
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
 include("connect.php");
 $email=$_SESSION["email"];
-$query_1="select * from dp_urls where email_id='".$email."'";
-$result_1=$conn->$query_1;
-$row_1=$result_1->fetch_assoc();
-$to_update=$row_1["facebook_url"];
+//$query_1=;
+$result_1=mysqli_query($conn,"select * from dp_urls where email_id='".$email."'");
+$row_1=mysqli_fetch_row($result_1);
+$to_update=$row_1[3];
 
-$query="update users set picture='".$to_update."'";
+$query="update users set picture='".$to_update."' where email_id='".$email."'";
+echo $query;
+echo $to_update;
+//die();
 mysqli_query($conn,$query);
 header("location:userprofile.php");
 mysqli_close($conn);
